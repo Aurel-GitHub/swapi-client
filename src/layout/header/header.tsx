3f42ad33f39d416115aa520c04def4e82de9c4ca
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2022-11-21 23:00:36 +0100

    add loginForm

--- src/layout/header/header.tsx
+++ src/layout/header/header.tsx
@@ -2,16 +2,17 @@ import DropdownCategories from '../../components/inputs/dropdown-categories';
 import InputLanguage from '../../components/inputs/input-language';
 import InputSearch from '../../components/inputs/input-search';
 import styles from './header.module.css';
+import '../../styles/global/global.css';
 import { Link } from 'react-router-dom';
 
 export default function Header(): JSX.Element {
   return (
     <header>
       <nav className={styles.navHeader}>
-        <Link to='/'>
+        <Link to='/' className='decorationNone'>
           <h1 className={styles.logo}>swapi</h1>
         </Link>
-        <Link to='/se-connecter'>
+        <Link to='/se-connecter' className='decorationNone'>
           <h1 className={styles.logo}>LOGIN TEST</h1>
         </Link>
         <InputSearch />
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,4 +1,36 @@
 import styles from './login.module.css';
+import { useForm } from 'react-hook-form';
+
+type LoginForm = {
+  firstname: string;
+  password: string;
+};
+
 export default function Login(): JSX.Element {
-  return <div className={styles.loginContainer}>login</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  function onSubmit(data: LoginForm): void {
+    return console.log(data);
+  }
+
+  return (
+    <form onSubmit={handleSubmit(() => onSubmit)} className={styles.formLogin}>
+      <label className={styles.formLabel}>Prénom</label>
+      <input className={styles.formInput} {...register('firstname', { required: true })} />
+      {errors.exampleRequired && <span>Ce champs est requis</span>}
+
+      <label className={styles.formLabel}>Mot de passe</label>
+      <input
+        type='password'
+        className={styles.formInput}
+        {...register('exampleRequired', { required: true })}
+      />
+      {errors.exampleRequired && <span>Ce champs est requis</span>}
+
+      <input type='submit' className={styles.formBtn} />
+    </form>
+  );
 }
