2ba271fc02846d0551592614bb8b5e7263fb6c3d
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2022-11-21 16:16:30 +0100

    add fontawesome & headerbtn

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import Footer from './components/layout/footer/footer';
 import Header from './components/layout/header/header';
-import styles from './styles/index.module.css';
 
 function App() {
   return (
