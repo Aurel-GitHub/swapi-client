06a048282d6e9afc0fefe654db982eafc8fd737d
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2022-11-22 22:54:06 +0100

    update store

--- src/app/store/Store.ts
+++ src/app/store/Store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../feature/UserSlice';
 import resultReducer from '../feature/ResultSlice';
 import spinnerReducer from '../feature/SpinnerSlice';
+import langReducer from '../feature/LangSlice';
 
 export const store = configureStore({
   reducer: {
     userConnected: userReducer,
     swapi: resultReducer,
     isLoading: spinnerReducer,
+    isWookieActived: langReducer,
   },
 });
 export type RootState = ReturnType<typeof store.getState>;
