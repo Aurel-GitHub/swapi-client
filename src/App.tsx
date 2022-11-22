6fb4b559e6e21938977e55f675d9882327eebb57
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2022-11-22 01:27:19 +0100

    add redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
-import Footer from './layout/footer/footer';
 import Header from './layout/header/header';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home/home';
 import Login from './pages/login/login';
 import ResultDetails from './pages/result-details/result-details';
+import { RequireAuth } from 'react-auth-kit';
 
 function App() {
   return (
     <>
       <div className='app'>
-        <Header />
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route
+            path='/'
+            element={
+              <RequireAuth loginPath='/se-connecter'>
+                <>
+                  <Header />
+                  <Home />
+                </>
+              </RequireAuth>
+            }
+          />
           <Route path='/se-connecter' element={<Login />} />
           <Route path='/:categories/:id' element={<ResultDetails />} />
         </Routes>
       </div>
-      <Footer />
     </>
   );
 }
--- src/components/forms/authentication/form-authentication.tsx
+++ src/components/forms/authentication/form-authentication.tsx
@@ -1,7 +1,8 @@
 import styles from './form-authentication.module.css';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { useState } from 'react';
-import axios, { AxiosError } from 'axios';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import { useSignIn } from 'react-auth-kit';
 
 export default function FormAuthentication(): JSX.Element {
@@ -19,6 +20,8 @@ export default function FormAuthentication(): JSX.Element {
 
   const signIn = useSignIn();
 
+  const naviguate = useNavigate();
+
   const onSubmit: SubmitHandler<Authentication> = async (
     formValues: Authentication,
   ): Promise<void> => {
@@ -31,36 +34,40 @@ export default function FormAuthentication(): JSX.Element {
         tokenType: 'Bearer',
         authState: { firstname: formValues.firstname },
       });
+      naviguate('/');
     } catch (error: any) {
       setErrorMessage(error?.response.data);
     }
   };
 
   return (
-    <form className={styles.formLogin} onSubmit={handleSubmit(onSubmit)}>
-      <label className={styles.formLabel}>Prénom</label>
-      <input
-        className={styles.formInput}
-        {...register('firstname', {
-          required: true,
-        })}
-      />
-      {errors.firstname && <small className={styles.textDanger}>Veuillez saisir un prénom</small>}
+    <>
+      <h1 style={{ textAlign: 'center' }}>Se connecter</h1>
+      <form className={styles.formLogin} onSubmit={handleSubmit(onSubmit)}>
+        <label className={styles.formLabel}>Prénom</label>
+        <input
+          className={styles.formInput}
+          {...register('firstname', {
+            required: true,
+          })}
+        />
+        {errors.firstname && <small className={styles.textDanger}>Veuillez saisir un prénom</small>}
 
-      <label className={styles.formLabel}>Mot de passe</label>
-      <input
-        type='password'
-        className={styles.formInput}
-        {...register('password', {
-          required: true,
-        })}
-      />
-      {errors.password && (
-        <small className={styles.textDanger}>Veuillez saisir un mot de passe</small>
-      )}
+        <label className={styles.formLabel}>Mot de passe</label>
+        <input
+          type='password'
+          className={styles.formInput}
+          {...register('password', {
+            required: true,
+          })}
+        />
+        {errors.password && (
+          <small className={styles.textDanger}>Veuillez saisir un mot de passe</small>
+        )}
 
-      {errorMessage && <small className={styles.textDanger}>{errorMessage}</small>}
-      <input type='submit' className={styles.formBtn} />
-    </form>
+        {errorMessage && <small className={styles.textDanger}>{errorMessage}</small>}
+        <input type='submit' className={styles.formBtn} />
+      </form>
+    </>
   );
 }
