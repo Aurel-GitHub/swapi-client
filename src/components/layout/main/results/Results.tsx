1fa489e4e26a24e45dc8ef536acd45e61cf313ff
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2022-11-22 15:44:07 +0100

    add result component

---
+++ src/components/layout/main/results/Results.tsx
@@ -0,0 +1,5 @@
+import styles from './Results.module.css';
+
+export default function Results(): JSX.Element {
+  return <div className={styles.resultSection}>Results</div>;
+}
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,10 @@
+import Results from '../../components/layout/main/results/Results';
 import styles from './Home.module.css';
 
 export default function Home(): JSX.Element {
   return (
     <div className={styles.homeContainer}>
-      <h3>Ici des résultats de recherches</h3>
+      <Results />
     </div>
   );
 }
